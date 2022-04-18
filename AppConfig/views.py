a347d02bc6027c8b499cc709522764362dbeb517
Cigan Oliviu David <ciganoliviudavid@gmail.com>
2022-04-18 13:15:42 +0300

    main: Craeate AppConfig

--- AppConfig/views.py
+++ AppConfig/views.py
@@ -2,4 +2,4 @@ from django.http import HttpResponse
 
 
 def index(request):
-    return HttpResponse('Setup Books')
+    return HttpResponse('Setup AppConfig')
--- BackendBestBooks/urls.py
+++ BackendBestBooks/urls.py
@@ -18,5 +18,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('Books.urls')),
+    path('books/', include('Books.urls')),
+    path('config/', include('AppConfig.urls')),
 ]
