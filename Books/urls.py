39258e142fc4b252cb6aba3981e681c7a6a4b6ba
Cigan Oliviu David <ciganoliviudavid@gmail.com>
2022-04-18 18:37:10 +0300

    main: Setup detail page view

--- Books/urls.py
+++ Books/urls.py
@@ -4,6 +4,7 @@ from rest_framework.urlpatterns import format_suffix_patterns
 from . import views
 
 urlpatterns = [
-    path('categories-lister', views.CategoriesLister.as_view(), name='CategoriesLister'),
+    path('categories-lister/', views.CategoriesLister.as_view(), name='CategoriesLister'),
+    path('categories-lister/<int:pk>', views.CategoriesDetails.as_view(), name='CategoriesDetails')
 ]
 urlpatterns = format_suffix_patterns(urlpatterns)
--- Books/views.py
+++ Books/views.py
@@ -1,3 +1,4 @@
+from django.http import Http404
 from rest_framework import status
 from rest_framework.response import Response
 from rest_framework.views import APIView
@@ -23,3 +24,34 @@ class CategoriesLister(APIView):
             return Response(serializer.data, status=status.HTTP_201_CREATED)
 
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+class CategoriesDetails(APIView):
+    @staticmethod
+    def get_post(pk):
+        try:
+            return Categorie.objects.get(pk=pk)
+        except:
+            raise Http404
+
+    def get(self, request, pk):
+        post = self.get_post(pk)
+        serializer = CategoriesSerializer(post)
+
+        return Response(serializer.data)
+
+    def put(self, request, pk):
+        post = self.get_post(pk)
+        serializer = CategoriesSerializer(post, data=request.data)
+
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def delete(self, request, pk):
+        post = self.get_post(pk)
+        post.delete()
+
+        return Response(status=status.HTTP_204_NO_CONTENT)
