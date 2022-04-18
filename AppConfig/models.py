26bac41fbbe3967f484b349abe8227387e41fa4d
Cigan Oliviu David <ciganoliviudavid@gmail.com>
2022-04-18 13:21:56 +0300

    main: Update models.py

--- AppConfig/models.py
+++ AppConfig/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+
+class Schema(models.Model):
+    route = models.CharField(max_length=50, required=True)
+    name = models.CharField(max_length=50, required=True)
+
+    def __str__(self):
+        return self.name
+
