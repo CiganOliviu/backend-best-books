2a45e9aa30b8b034ac9749bea73bdc9246d2b31e
Cigan Oliviu David <ciganoliviudavid@gmail.com>
2022-05-02 20:48:14 +0300

    main: Update admin fields

--- BackendBestBooks/settings.py
+++ BackendBestBooks/settings.py
@@ -129,7 +129,7 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'MEDIA')
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 CORS_ORIGIN_WHITELIST = [
-    '*'
+    # '*'
 ]
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedStaticFilesStorage'
--- Books/admin.py
+++ Books/admin.py
@@ -2,9 +2,18 @@ from django.contrib import admin
 
 from Books.models import *
 
+
+class AuthorAdmin(admin.ModelAdmin):
+    list_display = ('last_name', 'first_name', 'field')
+
+
+class BookAdmin(admin.ModelAdmin):
+    list_display = ('title', 'category', 'virtually_owned', 'physically_owned')
+
+
 admin.site.register(Categorie)
 admin.site.register(Nationalitie)
 admin.site.register(Field)
-admin.site.register(Author)
-admin.site.register(Book)
+admin.site.register(Author, AuthorAdmin)
+admin.site.register(Book, BookAdmin)
 
